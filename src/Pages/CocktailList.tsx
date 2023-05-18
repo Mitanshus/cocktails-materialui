80518d5676566d0a3918e3b8eeba0ff4043fa25c
Mitanshus <117567772+Mitanshus@users.noreply.github.com>
2023-05-18 11:52:03 +0530

    fixed link

--- src/Pages/CocktailList.tsx
+++ src/Pages/CocktailList.tsx
@@ -9,6 +9,7 @@ import {
 } from "@mui/material";
 import React from "react";
 import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
 
 interface Cocktail {
 	idDrink: string;
@@ -18,7 +19,6 @@ interface Cocktail {
 	strDrinkThumb: string;
 	strAlcoholic: string;
 }
-
 
 const CocktailList = () => {
 	var filtered = useSelector((state: any) => state.filteredCocktails);
@@ -61,19 +61,21 @@ const CocktailList = () => {
 
 									<Typography paragraph>{cocktail.strCategory}</Typography>
 									<Box sx={{ display: "flex", justifyContent: "center" }}>
-										<Button
-											variant='contained'
-											color='success'
-											href={`cocktail/${cocktail.idDrink}`}
-											sx={{
-												"&:hover": {
-													backgroundColor: "#cce6cc",
-													color: "#4da64d",
-												},
-											}}
-										>
-											Details
-										</Button>
+										<Link to={`cocktail/${cocktail.idDrink}`}>
+											<Button
+												variant='contained'
+												color='success'
+												// href={`cocktail/${cocktail.idDrink}`}
+												sx={{
+													"&:hover": {
+														backgroundColor: "#cce6cc",
+														color: "#4da64d",
+													},
+												}}
+											>
+												Details
+											</Button>
+										</Link>
 									</Box>
 								</Card>
 							</Box>
@@ -83,6 +85,5 @@ const CocktailList = () => {
 			</Grid>
 		</Container>
 	);
-	
 };
 export default CocktailList;
